c7152ca4fab70ffbe1296fa05d2b582f3f224049
Richard Ting <richard.tin@gmail.com>
2020-04-22 21:55:15 +0800

    feat(practice): 01.3 Initial commit

---
+++ 02.DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+}
