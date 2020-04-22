d9b36c3feaf85e107c30c80d63f21e992257f0da
Richard Ting <richard.tin@gmail.com>
2020-04-22 22:35:48 +0800

    feat(practice): 01.3 add and update image resources

--- 02.DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ 02.DiceRoller/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +18,15 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val randomInt = (1..6).random()
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
